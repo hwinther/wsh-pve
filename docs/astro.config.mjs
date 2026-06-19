a52c10513caa66857a59f030a147fb2d821310db
Hans Christian Winther-Sorensen <hc@wsh.no>
2026-06-19 05:42:53 +0200

    fix: Astro config format (#144)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
         },
         {
           label: "Reference",
-          autogenerate: { directory: "reference" },
+          items: [{ autogenerate: { directory: "reference" } }],
         },
       ],
     }),
